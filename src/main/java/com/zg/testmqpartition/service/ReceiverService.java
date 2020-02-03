f7873e090cb92dd9f6118c1f5bea0d16d8590cf7
StringHuangMac <StringHuangMac@mac.com>
2020-02-03 16:19:27 +0800

    单实例多线程消费消息

--- src/main/java/com/zg/testmqpartition/service/ReceiverService.java
+++ src/main/java/com/zg/testmqpartition/service/ReceiverService.java
@@ -8,22 +8,39 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.*;
+
 @Log4j2
 @EnableBinding(value = InboundChannel.class)
 @Service
 public class ReceiverService {
+
+    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+            8,
+            16,
+            1000,
+            TimeUnit.MILLISECONDS,
+            new LinkedBlockingDeque<Runnable>(),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.CallerRunsPolicy()
+    );
 
     @Value("${server.port}")
     private String port;
 
     @StreamListener(InboundChannel.INBOUND_CHANNEL_NAME)
     public void receiveMsg(Message<String> msg) throws Exception {
-        long sleepSeconds = Double.valueOf(Math.random() * 10000).longValue();
-        try {
-            Thread.sleep(sleepSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        log.info("ReceiverClass in Instance {}  has received message: {}, and sleep {} millis", port, msg.getPayload(), sleepSeconds);
+        threadPoolExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                long sleepSeconds = Double.valueOf(Math.random() * 10000).longValue();
+                try {
+                    Thread.sleep(sleepSeconds);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                log.info("ReceiverClass in Instance {}  has received message: {}, and sleep {} millis", port, msg.getPayload(), sleepSeconds);
+            }
+        });
     }
 }
